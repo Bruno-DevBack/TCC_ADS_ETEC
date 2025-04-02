1f301bd6f0aaaca7628d026442ff9f1e0c3e65e6
ningolly <nicollydev@gmail.com>
2025-04-02 16:45:35 -0300

    Retoques NavBar

--- Renix/src/app/page.tsx
+++ Renix/src/app/page.tsx
@@ -1,65 +1,92 @@
-import Image from "next/image";
+import { Disclosure, DisclosureButton, DisclosurePanel, Menu, MenuButton, MenuItem, MenuItems } from '@headlessui/react'
+import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/react/24/outline'
+
+const navigation = [
+  { name: 'Sobre', href: '#', current: false },
+  { name: 'Serviços', href: '#', current: false },
+  { name: 'Contato', href: '#', current: false },
+]
+
+function classNames(...classes: (string | undefined | null | boolean)[]) {
+  return classes.filter(Boolean).join(' ')
+}
+
 
 export default function Home() {
   return (
-    <nav>
-      <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="relative flex h-4 items-center justify-between">
-          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            {/* Mobile menu button*/}
-            <button type="button" className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset" aria-controls="mobile-menu" aria-expanded="false">
-              <span className="absolute -inset-0.5"></span>
-              <span className="sr-only">Open main menu</span>
-              {/*
-            Icon when menu is closed.
-            Menu open: "hidden", Menu closed: "block"
-            */}
-              <svg className="block size-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-              </svg>
-              {/*
-            Icon when menu is open.
-            Menu open: "block", Menu closed: "hidden"
-          */}
-              <svg className="hidden size-6" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" aria-hidden="true" data-slot="icon">
-                <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
-              </svg>
-            </button>
-          </div>
-          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
-            <div className="flex shrink-0 items-center">
-              <Image
-                className="h-60 w-auto"
-                src="/1.png"
-                width={200}
-                height={200}
-                alt="Your Company" />
+    <>
+      <Disclosure as="nav" className="bg-whitesmoke-800">
+        <div className="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
+          <div className="relative flex h-16 items-center justify-between">
+            <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+              {/* Mobile menu button*/}
+              <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:ring-2 focus:ring-white focus:outline-hidden focus:ring-inset">
+                <span className="absolute -inset-0.5" />
+                <span className="sr-only">Open main menu</span>
+                <Bars3Icon aria-hidden="true" className="block size-6 group-data-open:hidden" />
+                <XMarkIcon aria-hidden="true" className="hidden size-6 group-data-open:block" />
+              </DisclosureButton>
             </div>
-            <div className="hidden sm:ml-6 sm:block">
-              <div className="flex space-x-4">
-                {/*current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white"*/}
-                <a href="#" className="px-3 py-2 text-sm font-medium text-white" aria-current="page">Sobre</a>
-                <a href="#" className="px-3 py-2 text-sm font-medium text-gray-300 hover:hover:text-white">Serviços</a>
-                <a href="#" className="px-3 py-2 text-sm font-medium text-gray-300 hover:hover:text-white">Contatos</a>
+            <div className="flex items-center mt-15">
+              <div className="flex shrink-0 items-center">
+                <img
+                  alt="Your Company"
+                  src="/1.png"
+                  className="h-58 w-auto"
+                />
+              </div>
+              <div className="hidden sm:mr-8 sm:block">
+                <div className="flex space-x-4">
+                  {navigation.map((item) => (
+                    <a
+                      key={item.name}
+                      href={item.href}
+                      aria-current={item.current ? 'page' : undefined}
+                      className={classNames(
+                        item.current ? 'bg-gray-900 text-black' : 'text-black-300 hover:bg-gray-700 hover:text-white',
+                        'rounded-md px-3 py-2 text-lg font-extrabold',
+                      )}
+                    >
+                      {item.name}
+                    </a>
+                  ))}
+                </div>
               </div>
             </div>
           </div>
-          <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+        </div>
+
+        <DisclosurePanel className="sm:hidden">
+          <div className="space-y-1 px-2 pt-2 pb-3">
+            {navigation.map((item) => (
+              <DisclosureButton
+                key={item.name}
+                as="a"
+                href={item.href}
+                aria-current={item.current ? 'page' : undefined}
+                className={classNames(
+                  item.current ? 'bg-gray-900 text-white' : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                  'block rounded-md px-3 py-2 text-base font-medium',
+                )}
+              >
+                {item.name}
+              </DisclosureButton>
+            ))}
           </div>
+        </DisclosurePanel>
+      </Disclosure>
+      <main className="mt-50 sm:px-20 md:px-12 lg:px-20 xl:px-32 2xl:px-90">
+        <div className="mt-50 px-25 sm:px-20 md:px-12 lg:px-20 xl:px-32 2xl:px-7">
+          <h1 className="text-black font-extrabold sm:text-xs 2xl:text-4xl">
+            Invista
+          </h1>
+          <button className="bg-[#028264] text-white mt-10 px-10 py-2 rounded">
+            <h1 className="text-white font-extrabold">
+              INVESTIR
+            </h1>
+          </button>
         </div>
-      </div>
-
-      {/*Mobile menu, show/hide based on menu state.*/}
-      <div className="sm:hidden" id="mobile-menu">
-        <div className="space-y-1 px-2 pt-2 pb-3">
-          {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white"*/}
-          <a href="#" className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white" aria-current="page">Dashboard</a>
-          <a href="#" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Team</a>
-          <a href="#" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Projects</a>
-          <a href="#" className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white">Calendar</a>
-        </div>
-      </div>
-    </nav>
-
-  );
+      </main>
+    </>
+  )
 }
