777bcd278afcdfc4152322bd75ea9fc3314266a3
ArthurArgeri <arthur.argeri@outlook.com>
2025-05-05 17:04:59 -0300

    Responsividade dos Investimentos e Criação

--- Renix/src/app/investment/page.tsx
+++ Renix/src/app/investment/page.tsx
@@ -4,9 +4,11 @@ import { useRouter } from 'next/navigation';
 
 
 export default function Dashboard() {
+
     const router = useRouter();
 
     return (
+
         <div className="flex min-h-screen">
 
             {/* Conteúdo principal */}
@@ -27,30 +29,9 @@ export default function Dashboard() {
                         <img src="/avatar.png" alt="Avatar" className="w-12 h-12 rounded-full" />
                     </div>
                 </nav>
-
-                {/* Conteúdo */}
-                <main className="flex-1 bg-gray-50 p-6">
-                    <div className="flex items-center justify-between mb-5">
-                        <h1 className="text-2xl md:text-3xl font-bold text-black-500">
-                            Seus Investimentos
-                        </h1>
-                        <button
-                            onClick={() => router.push('/newinvestment')}
-                            className="bg-gray-600 hover:bg-green-700 text-white font-bold py-2 px-4 md:py-3 md:px-5 rounded-full text-lg lg:text-xl"
-                        >
-                            +
-                        </button>
-                    </div>
-
-                    <div className=" grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4">
-                        <div className="bg-white shadow rounded-lg p-4">
-                            <h3 className="text-sm md:text-md lg:text-lg font-medium text-gray-500">Nubank</h3>
-                            <p className="text-2xl md:text-3xl font-bold text-gray-900">1.200</p>
-                        </div>
-                    </div>
-                </main>
             </div>
         </div>
-
     );
 }
+
+
--- Renix/src/app/newinvestment/page.tsx
+++ Renix/src/app/newinvestment/page.tsx
@@ -14,44 +14,70 @@ export default function Dashboard() {
                 {/* Navbar */}
                 <nav className="h-16 bg-white border-b border-gray-200 px-6 flex items-center justify-between">
                     <div className="flex items-center space-x-4">
-                        <img src="/logo.png" alt="Logo" className="w-14 h-14" />
+                        <img src="/logo.png" alt="Logo" className="w-12 h-12" />
                     </div>
 
                     {/* Título centralizado */}
-                    <h1 className="text-lg font-extrabold text-black-500 absolute left-1/2 transform -translate-x-1/2">
+                    <h1 className="text-2xl md:text-3xl font-extrabold text-black-500 absolute left-1/2 transform -translate-x-1/2">
                         RENIX
                     </h1>
 
                     <div className="flex items-center space-x-4">
-                        <span className="text-sm hidden sm:block">Olá, Nome</span>
-                        <img src="/avatar.png" alt="Avatar" className="w-8 h-8 rounded-full" />
+                        <span className="text-md lg:text-lg hidden sm:block">Olá, Nome</span>
+                        <img src="/avatar.png" alt="Avatar" className="w-12 h-12 rounded-full" />
                     </div>
                 </nav>
 
                 {/* Conteúdo */}
                 <main className="min-h-screen bg-gray-50 flex justify-center p-6">
-                    <div className="flex flex-col items-center space-y-6">
+                    <div className=" flex flex-col items-center space-y-6 lg:-mt-20">
                         {/* Título centralizado */}
-                        <h1 className="text-2xl font-bold text-black-500 mt-20">Novo Investimento</h1>
+                        <h1 className="text-3xl font-bold text-black-500 mt-20">Novo Investimento</h1>
 
                         {/* Card com inputs */}
                         <div className="bg-white shadow rounded-lg p-6 w-full max-w-md">
                             <form className="space-y-4">
+
                                 <div>
-                                    <label className="block text-sm font-medium text-gray-700">Valor</label>
+                                    <label className="block text-sm sm:text-md font-medium text-gray-700 mb-1">Banco</label>
+                                    <select
+                                        required
+                                        className="text-sm sm:text-md mt-1 block w-full rounded-md border-gray-300 border-1 shadow-sm focus:border-blue-500 focus:ring-blue-500 py-1 px-1"
+                                    >
+                                        <option value="">Selecione um banco</option>
+                                        <option value="nubank">Nubank</option>
+                                        <option value="bancodobrasil">Banco do Brasil</option>
+                                    </select>
+                                </div>
+
+
+                                <div>
+                                    <label className="block text-sm sm:text-md font-medium text-gray-700 mb-1">Valor</label>
                                     <input
+                                        required
                                         type="number"
-                                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500"
+                                        className="text-sm sm:text-md mt-1 block w-full rounded-md border-gray-300 border-1 shadow-sm focus:border-blue-500 focus:ring-blue-500 py-1 px-1"
                                         placeholder="Digite o valor"
                                     />
                                 </div>
 
                                 <div>
-                                    <label className="block text-sm font-medium text-gray-700">Tempo (meses)</label>
+                                    <label className="block text-sm sm:text-md font-medium text-gray-700 mb-1">Tempo (meses)</label>
                                     <input
-                                        type="number"
-                                        className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500"
-                                        placeholder="Ex: 12"
+                                        required
+                                        type="date"
+                                        className="text-sm sm:text-md mt-1 block w-full rounded-md border-gray-300 border-1 shadow-sm focus:border-blue-500 focus:ring-blue-500 py-1 px-1"
+                                        placeholder="Ex: 05/05/2025"
+                                    />
+                                </div>
+
+                                <div>
+                                    <label className="block text-sm sm:text-md font-medium text-gray-700 mb-1">Tempo (meses)</label>
+                                    <input
+                                        required
+                                        type="date"
+                                        className="text-sm sm:text-md mt-1 block w-full rounded-md border-gray-300 border-1 shadow-sm focus:border-blue-500 focus:ring-blue-500 py-1 px-1"
+                                        placeholder="Ex: 25/12/2026"
                                     />
                                 </div>
 
